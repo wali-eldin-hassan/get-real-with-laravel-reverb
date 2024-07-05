bd3f33fa86446ce7cedd31966430316aaa024d89
Joe Dixon <hello@joedixon.co.uk>
2024-07-05 21:04:19 +0100

    update users

--- resources/views/components/users.blade.php
+++ resources/views/components/users.blade.php
@@ -1,25 +1,51 @@
-<ul x-cloak x-show="open">
-    @foreach ($users as $user)
+<ul x-cloak x-data="users" x-show="open">
+    <template x-for="user in users">
         <li>
-            <span class="flex items-center gap-x-2 rounded-md px-4 py-1 hover:bg-fuchsia-900 hover:text-white">
+            <span
+                class="flex items-center gap-x-2 rounded-md px-4 py-1 hover:bg-fuchsia-900 hover:text-white"
+            >
                 <span class="relative">
                     <img
-                        src="{{ $user->avatar }}"
-                        alt="{{ $user->name }}"
+                        :src="user.avatar"
+                        :alt="user.name"
                         class="h-6 w-6 rounded-md"
                     />
 
                     <span class="absolute -bottom-1 -right-1">
-                        <span class="flex h-3.5 w-3.5 items-center justify-center rounded-full bg-sidebar">
+                        <span
+                            class="flex h-3.5 w-3.5 items-center justify-center rounded-full bg-sidebar"
+                        >
                             <span
-                                class="flex h-2 w-2 rounded-full border border-gray-400 bg-sidebar"
+                                class="flex h-2 w-2 rounded-full border bg-sidebar border-gray-400"
                             ></span>
                         </span>
                     </span>
                 </span>
 
-                {{ $user->name }}
+                <span x-text="user.name"></span>
             </span>
         </li>
-    @endforeach
+    </template>
 </ul>
+
+@script
+<script>
+Alpine.data('users', () => {
+    return {
+        users: @js($users),
+        init() {
+        },
+        markOnline(users) {
+            users.forEach((user) => {
+                this.users.find((u) => u.id === user.id).online = true
+            })
+        },
+        markOffline(users) {
+            users.forEach((user) => {
+                this.users.find((u) => u.id === user.id).online = false
+            })
+        },
+    }
+});
+</script>
+@endscript
